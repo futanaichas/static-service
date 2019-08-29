800605f5d133e4c87996ae91268cc73434733773
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-29 19:01:15 +0800

    优化代码

--- src/handle/upload.go
+++ src/handle/upload.go
@@ -1,53 +1,14 @@
 package handle
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"path/filepath"
-	"time"
 
 	"github.com/futanaichas/static-service/src/module"
 )
 
-// NowDate ...
-func NowDate() string {
-	return time.Now().Format("2006-01-02")
-}
-
 func getFormValue(r *http.Request) (string, string) {
 	return r.FormValue("type"), r.FormValue("info")
-}
-
-func hanleFileName(data []byte, fileType string, fileInfo string) (hashstr string, fileDate string) {
-	hash := md5.Sum(data)
-	hashstr = hex.EncodeToString(hash[:])
-	fileDate = NowDate()
-	return
-}
-
-// SaveFile ...
-func SaveFile(fileName string, rootPath string, time string, data []byte) error {
-	os.Mkdir(filepath.Join(rootPath, time), os.ModePerm)
-
-	file, err := os.Create(filepath.Join(rootPath, time, fileName))
-	if err != nil {
-		fmt.Println(filepath.Join(rootPath, time, fileName))
-		return errors.New("create file fail")
-	}
-	defer file.Close()
-
-	_, err = file.Write(data)
-	if err != nil {
-		fmt.Println("write file fail")
-		return errors.New("write file fail")
-	}
-
-	return nil
 }
 
 // Upload 上传处理
@@ -81,16 +42,19 @@ func Upload() http.HandlerFunc {
 		}
 
 		//重命名文件名称
-		hash, fileDate := hanleFileName(fileByte, fileType, fileInfo)
+		uploadFile := &File{
+			Type: fileType,
+			Info: fileInfo,
+			Data: fileByte,
+		}
+		uploadFile.SetRoot("./public/static")
 
-		// 储存文件
-		rootPath, _ := filepath.Abs("./public/static")
-		err = SaveFile(fmt.Sprintf("%s%s.%s", hash, fileInfo, fileType), rootPath, fileDate, fileByte)
+		err = uploadFile.SaveFile()
 		if err != nil {
 			w.Write(module.FailRes())
 			return
 		}
 
-		w.Write(module.OkRes(hash, fmt.Sprintf("/static/%s/%s%s.%s", fileDate, hash, fileInfo, fileType)))
+		w.Write(module.OkRes(uploadFile.CheckHash(), uploadFile.URL()))
 	}
 }
--- tests/savefile_test.go
+++ tests/savefile_test.go
@@ -1,11 +1,7 @@
 package tests
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
 	"math/rand"
-	"path/filepath"
 	"testing"
 	"time"
 
@@ -13,15 +9,20 @@ import (
 )
 
 func TestSaveFile(t *testing.T) {
-	root, _ := filepath.Abs("./")
 	var data []byte
 	rand.Seed(time.Now().Unix())
 	for i := 0; i < rand.Intn(100); i++ {
 		data = append(data, 'a')
 	}
-	hash := md5.Sum(data)
-	fmt.Println(filepath.Join(root, hex.EncodeToString(hash[:])+".txt"))
-	err := handle.SaveFile(hex.EncodeToString(hash[:])+".txt", root, handle.NowDate(), data)
+
+	uploadFile := &handle.File{
+		Type: "txt",
+		Info: "@test",
+		Data: data,
+	}
+	uploadFile.SetRoot("./")
+
+	err := uploadFile.SaveFile()
 	if err != nil {
 		t.Fail()
 	}
