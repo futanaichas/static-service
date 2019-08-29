ef8928afa2350538457097ab2478da25b7d13daf
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-29 17:07:06 +0800

    添加docker网络

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 git pull https://github.com/futanaichas/static-service.git && cd static-service
 docker build -t futanaicha/static-service .
+docker network create futanaicha
 mkdir -p /var/www/futanaicha/public
 docker run -d \
 -p 3000:3000 \
---name futanaicha-static-service \
+--net futanaicha
+--name static-service \
 -v /var/www/futanaicha/public: /var/www/futanaicha/public
 futanaicha/static-service
