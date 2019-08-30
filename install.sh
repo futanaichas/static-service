ab5ef43fb903fb9b67cc1d0ed2b3f2e9354aa3ed
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-30 21:39:59 +0800

    更新路径

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 git pull
 docker build -t futanaicha/static-service .
 docker network create futanaicha
-mkdir -p /var/www/futanaicha/public
+mkdir -p ~/devopt/wwwroot/static
 docker run -d \
 -P 80 \
 --net futanaicha
 --name static-service \
--v /var/www/futanaicha/public:/var/www/futanaicha/public \
+-v ~/devopt/wwwroot/:/var/www/futanaicha/public \
 futanaicha/static-service
