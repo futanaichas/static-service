5e28587db43c00a1369a1f830647612fd00e4970
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-31 00:21:22 +0800

    添加子模块

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ docker build -t futanaicha/static-service .
 docker network create futanaicha
 mkdir -p ~/devopt/wwwroot/static
 docker run -d \
--p 8080:80 \
 --net futanaicha \
 --name static-service \
 -v ~/devopt/wwwroot/:/var/www/futanaicha/public \
